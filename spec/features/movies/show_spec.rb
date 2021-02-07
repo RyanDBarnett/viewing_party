8dfc70ad7b70eab1a8b9a386da4f8671c3a48c28
Adam Etzion <60630167+aetzion1@users.noreply.github.com>
2021-02-07 14:37:51 -0700

    Update show_spec.rb
    13 minutes not 12 in expectation

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'movies show page', type: :feature do
                 visit movie_path(2)
                 expect(page).to have_content('Ariel')
                 expect(page).to have_content('Vote Average: 6.8')
-                expect(page).to have_content("Runtime: 1 Hour(s) 12 Minute(s)")
+                expect(page).to have_content("Runtime: 1 Hour(s) 13 Minute(s)")
                 expect(page).to have_content("Genres: Drama, Crime, Comedy")
                 expect(page).to have_content("Taisto Kasurinen is a Finnish coal miner whose father has just committed suicide and who is framed for a crime he did not commit. In jail, he starts to dream about leaving the country and starting a new life. He escapes from prison but things don't go as planned...")
 
