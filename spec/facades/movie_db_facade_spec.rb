07db1d372e4d76ae117b296909ef392e6bb6e412
Adam Etzion <adametzion90@gmail.com>
2021-02-08 09:54:01 -0700

    Add search_films test in facade spec

--- spec/facades/movie_db_facade_spec.rb
+++ spec/facades/movie_db_facade_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe MovieDbFacade do
-  it 'processes movie data and returns 40 Movie objects', :vcr do
-    films = MovieDbFacade.discover_films
+  describe 'happy path' do
+    it 'processes movie data and returns 40 Movie objects', :vcr do
+      films = MovieDbFacade.discover_films
 
-    expect(films.count).to eq(40)
-    films.each do |film|
-      expect(film).to be_an_instance_of(Film)
+      expect(films).to be_an(Array)
+      expect(films.count).to eq(40)
+      expect(films[0]).to be_a(Film)
+    end
+
+    it 'processes movie data and searches for films', :vcr do
+      query = 'Elf'
+
+      films = MovieDbFacade.search_films(query)
+
+      expect(films).to be_an(Array)
+      expect(films[0]).to be_a(Film)
     end
   end
 end
