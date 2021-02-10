8046eff5feda872df40f49252d65acaf465d7335
Phil McCarthy <philmcc2631@gmail.com>
2021-02-10 08:42:01 -0700

    Resolve existing duplicate friend issue by calling all_friends in flow control

--- app/facades/friendships_facade.rb
+++ app/facades/friendships_facade.rb
@@ -3,7 +3,7 @@ class FriendshipsFacade
     def add_friendship(current_user, friend)
       if friend.id == current_user.id
         'Failed Friend Request. Cannot Friend Yourself!'
-      elsif current_user.friends.include?(friend)
+      elsif current_user.all_friends.include?(friend)
         "Failed Friend Request. Already Friended User: #{friend.name}"
       else
         current_user.friendships.create!({ friend_id: friend.id })
