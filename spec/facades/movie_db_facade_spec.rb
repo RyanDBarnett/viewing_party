94709a702906dc9ac89f72de18cf12a92b3c9cec
Phil McCarthy <philmcc2631@gmail.com>
2021-02-10 16:09:30 -0700

    Remove comments and clean up whitespace

--- spec/facades/movie_db_facade_spec.rb
+++ spec/facades/movie_db_facade_spec.rb
@@ -2,16 +2,6 @@ require 'rails_helper'
 
 RSpec.describe MovieDbFacade do
   describe 'happy path' do
-    # let (:film) do 
-    #   mdb_id = 10719
-    #   VCR.use_cassette("happy_path/film") { MovieDbFacade.get_movie_info(mdb_id) }
-    # end
-
-    # let(:film_search) do
-    #   query = "Elf"
-    # VCR.use_cassette("happy_path/search_films") { MovieDbFacade.search_films(query) }
-    # end
-
     it 'discovers top 40 films', :vcr do
       films = MovieDbFacade.discover_films
 
@@ -31,25 +21,17 @@ RSpec.describe MovieDbFacade do
     
     it 'gets film data', :vcr do
       mdb_id = 10719
-      film= MovieDbFacade.get_movie_info(mdb_id)
+      film = MovieDbFacade.get_movie_info(mdb_id)
       expect(film).to be_a(Film)
     end
   end
 
   describe 'sad path' do
-    # let(:film_search) do
-    #   query = 'Elf'
-    #   VCR.use_cassette("happy_path/search_films") { MovieDbFacade.search_films(query) }
-    # end
-  #     query = "Asgjhhs hs68"
-  #     VCR.use_cassette("happy_path/search_films") { MovieDbFacade.search_films(query) }
-  #   end
-
     it 'returns nil if no match is found in search', :vcr do
       query = 'Asgjhhs hs68'
       film_search = MovieDbFacade.search_films(query)
 
       expect(film_search).to eq([])
-    end  
+    end
   end
 end
